63b45911efd61969793008b99b68837ad72d5fa2
ra72e6 <ra72e6@hanmail.net>
2022-10-20 10:02:14 +0900

    6th

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,6 @@ import { Store } from '../utils/Store';
 export default function Layout({ title, children }) {
   const { state } = useContext(Store);
   const { cart } = state;
-  console.log(cart);
 
   return (
     <>
