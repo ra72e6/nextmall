93ea6e2dc2702d2860bc63f03cee5d6bbfd2ce22
ra72e6 <ra72e6@hanmail.net>
2022-10-25 12:43:09 +0900

    8th

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,10 @@ import { Store } from '../utils/Store';
 export default function Layout({ title, children }) {
   const { state } = useContext(Store);
   const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
 
   return (
     <>
@@ -27,6 +31,7 @@ export default function Layout({ title, children }) {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </span>
                   )}
