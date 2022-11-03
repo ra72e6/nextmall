34e43dec9f515fbf56c661f95bb32e1611ef0e9f
ra72e6 <ra72e6@hanmail.net>
2022-11-03 10:23:26 +0900

    11th

---
+++ pages/api/seed.js
@@ -0,0 +1,15 @@
+import User from '../../models/User';
+import data from '../../utils/data';
+import db from '../../utils/db';
+
+const handler = async (req, res) => {
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await db.disconnect();
+  res.send({
+    message: 'seeded successfully. 초기 사용자를 성공적으로 등록했습니다.',
+  });
+};
+
+export default handler;
