286afc9db5d9281526bd7f6d4c6a47231f166e93
ra72e6 <ra72e6@hanmail.net>
2022-11-15 12:15:44 +0900

    14th

---
+++ components/CheckoutWizard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function CheckoutWizard({ activeStep = 0 }) {
+  const steps = [
+    'User login',
+    'Shipping Address',
+    'Payment Method',
+    'Place Order',
+  ];
+
+  return (
+    <div className="mb-5 flex flex-wrap">
+      {steps.map((step, index) => (
+        <div
+          key={step}
+          className={`flex-1 border-b-2 text-center ${
+            index <= activeStep
+              ? 'border-indigo-500 text-indigo-500'
+              : 'border-gray-400 text-gray-400'
+          }`}
+        >
+          {step}
+        </div>
+      ))}
+    </div>
+  );
+}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Router, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
@@ -8,15 +8,15 @@ import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
-  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
+  console.log(product);
 
   if (!product) {
     return <div>Product Not Found. 그런 상품이 없습니다.</div>;
   }
-
+  const router = useRouter();
   const addToCartHandler = () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
